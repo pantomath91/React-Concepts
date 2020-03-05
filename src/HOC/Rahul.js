9fb62ca5b8ec70e59bbea14c44bbd73a02cea924
Sachin Lohani <schnlohani>
2020-03-05 18:15:15 +0530

    minor editing

--- src/HOC/Rahul.js
+++ src/HOC/Rahul.js
@@ -6,7 +6,7 @@ class Rahul extends Component {
     return (
       
     <div>
-      <h2>{this.props.camp}</h2>
+      <h2>Camp: {this.props.camp}</h2>
       <h3 onMouseOver={()=>{this.props.hocgunshots}}>Rahul {this.props.hochandleGunshots} Gunshots: {this.props.gunshots}</h3>
       {/* console.log(this.props.camp) */}
       </div>
