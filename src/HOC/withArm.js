be1792b2adc91d20cae38ba2aab49411b545c7da
Sachin Lohani <slohani@netspend.com>
2021-04-28 12:34:39 +0530

    fixing

--- src/HOC/withArm.js
+++ src/HOC/withArm.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
-const Army = (Men,shot) =>{
-    class NewMen extends Component{
+const Army = (Men, shot) => {
+    class NewMen extends Component {
         state = {
-            gunshots:0
+            gunshots: 0
         }
-    
-        handleGunshots=()=>{
-          this.setState({gunshots: this.state.gunshots+shot})
+
+        handleGunshots = () => {
+            this.setState({ gunshots: this.state.gunshots + shot })
         }
-        render(){
-            console.log(this.props.camp)
-            return<Men hocgunname="AK47" hocgunshots={this.state.gunshots} hochandleGunshots={this.handleGunshots} {...this.props}/>
+        render() {
+            return (<Men hocgunname="AK47" hocgunshots={this.state.gunshots} hochandleGunshots={this.handleGunshots} {...this.props} />)
         }
     }
     return NewMen;
